faf2f585a159d2916609df8aa816f8fe47758f10
Jered Adkins <jeredkins@gmail.com>
2022-03-23 11:23:19 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css'
 import Home from './components/home/Home'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
-import Projects from './components/projects/Projects'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import TopButton from './components/topButton/TopButton'
@@ -14,7 +13,6 @@ function App() {
       <Home />
       <About />
       <Experience />
-      <Projects />
       <Contact />
       <Footer />
       <TopButton />
